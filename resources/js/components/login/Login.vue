a64117296968c379a155a3bda46219fb16d0c845
awais512 <raz.awais25@gmail.com>
2019-11-26 14:44:29 +0500

    Getting Access Token from Login Api

--- resources/js/components/login/Login.vue
+++ resources/js/components/login/Login.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Login</h1>
+  <v-container>
+    <v-form @submit.prevent="login">
+      <v-text-field label="Email" v-model="form.email" type="email" required></v-text-field>
+
+      <v-text-field label="Password" v-model="form.password" type="password" required></v-text-field>
+
+      <v-btn type="submit" color="green">Login</v-btn>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post("/api/auth/login", this.form)
+        .then(res => console.log(res.data))
+        .catch(error => console.log(error.response.data));
+    }
+  }
+};
 </script>
 
 <style>

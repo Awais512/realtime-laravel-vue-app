c71b06e6c4c77347935b8e5111c04a99d59c6a1c
awais512 <raz.awais25@gmail.com>
2019-11-26 13:36:52 +0500

    Login Component Part 1

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <toolbar></toolbar>
+    <router-view></router-view>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import toolbar from "./Toolbar";
+import Login from "../components/login/Login";
 import AppFooter from "./AppFooter";
 export default {
-  components: { toolbar, AppFooter }
+  components: { toolbar, AppFooter, Login }
 };
 </script>
 
--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -5,12 +5,23 @@
 
       <v-spacer></v-spacer>
 
-      <v-toolbar-items>
-        <v-btn text>Forum</v-btn>
-        <v-btn text>Ask Question</v-btn>
-        <v-btn text>Category</v-btn>
-        <v-btn text>Login</v-btn>
-      </v-toolbar-items>
+      <div class="hidden-sm-and-down">
+        <router-link to="/forum">
+          <v-btn text>Forum</v-btn>
+        </router-link>
+
+        <router-link to="/ask_question">
+          <v-btn text>Ask Question</v-btn>
+        </router-link>
+
+        <router-link to="category">
+          <v-btn text>Category</v-btn>
+        </router-link>
+
+        <router-link to="/login">
+          <v-btn text>Login</v-btn>
+        </router-link>
+      </div>
 
       <template v-if="$vuetify.breakpoint.smAndUp">
         <v-btn icon>
